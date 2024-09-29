a9d51e3231e2ab1f9509ebc9efda4cf691899bb9
ichibha <ichibha@icloud.com>
2024-09-29 10:20:22 +0900

    modified and refactored get-dielectric-tensor

--- lib.py
+++ lib.py
@@ -49,48 +49,6 @@ def get_pymatgen_structure(poscar_path: str):
         raise RuntimeError(f"Failed to read {poscar_path}: {e}")
 
 
-def get_epsilon(vasprun_path):
-    # XMLファイルをパースし、そのルート要素を取得する
-    root = ET.parse(vasprun_path).getroot()
-
-    # XML構造内で誘電率テンソルを含むarray要素を取得する
-    epsilon_elements = root.findall(".//varray[@name='epsilon']/v")
-
-    # 抽出した誘電率テンソルを格納するための空リストを初期化する
-    epsilon_array: list[list[float]] = []
-
-    # epsilon_elementsから各行を取得し、floatリストに変換し、epsilon_arrayに追加する
-    for element in epsilon_elements:
-        epsilon_array.append(list(map(float, element.text.split())))
-
-    # 誘電率テンソルを3x3のfloat配列として返す
-    return epsilon_array
-
-
-def get_born_charges(vasprun_path):
-    # vasprun.xmlファイルをパースし、そのルート要素を取得する
-    root = ET.parse(vasprun_path).getroot()
-
-    # XML構造内でボルン有効電荷テンソルを含むarray要素を取得する
-    born_charge_elements_array = root.findall(".//array[@name='born_charges']/set")
-
-    # 抽出したボルン有効電荷テンソルを格納するための空リストを初期化する
-    born_charges: list[list[list[float]]] = []
-
-    # 原子ごとにループする
-    # born_charge_elementsは当該原子のボルン有効電荷テンソルである
-    for born_charge_elements in born_charge_elements_array:
-        # 所与の原子のボルン有効電荷テンソルを格納するリストを初期化する
-        born_charges.append([])
-
-        # born_charge_elementsから各行を取得し、floatリストに変換し、初期化したリストに追加する
-        for element in born_charge_elements.findall("v"):
-            born_charges[-1].append(list(map(float, element.text.split())))
-
-    # ボルン有効電荷テンソルのリストを返す。
-    return born_charges
-
-
 def calculate_debye_temperature(debye_frequency_thz: float):
     debye_frequency_hz = debye_frequency_thz * 1e12
     debye_temperature = const.h * debye_frequency_hz / const.Boltzmann
