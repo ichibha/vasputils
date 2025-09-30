52e398733a5d59b2ea723b56d9d5c48c88a55b13
ichibha <ichibha@icloud.com>
2025-09-30 12:03:48 -0400

    デコレータのインポートパスを修正し、`prepare-phonopy-inputs`に新しい機能を追加。`makefile`に新しい送信先を追加。

---
+++ lib/vasputils/decorator.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import argparse
+import functools
+
+from pymatgen.io.vasp import Vasprun
+
+
+def process_vasprun_decorator(description):
+    def decorator(process_vasprun):
+        @functools.wraps(process_vasprun)
+        def wrapper():
+            parser = argparse.ArgumentParser(description=description)
+            parser.add_argument("vasprun_path", type=str, help="vasprun.xml path")
+            args = parser.parse_args()
+            vasprun = Vasprun(args.vasprun_path)
+            return process_vasprun(vasprun)
+
+        return wrapper
+
+    return decorator
+
+
+def process_poscar_decorator(description):
+    def decorator(process_poscar):
+        @functools.wraps(process_poscar)
+        def wrapper():
+            parser = argparse.ArgumentParser(description=description)
+            parser.add_argument("poscar_path", type=str, help="POSCAR path")
+            args = parser.parse_args()
+            return process_poscar(args.poscar_path)
+
+        return wrapper
+
+    return decorator
+
+
+# def search_directories(pattern: str, root_directory: str):
+#     directories = [
+#         os.path.abspath(path)
+#         for path in glob(os.path.join(root_directory, "*"))
+#         if os.path.isdir(path) and re.match(pattern, os.path.basename(path))
+#     ]
+#     return sorted(directories)
+
+
+# def search_numbered_directories(root_directory: str):
+#     return search_directories("^[0-9]+$", root_directory)
--- makefile
+++ makefile
@@ -1,2 +1,3 @@
 send:
-	rsync -av --exclude=".*" --exclude="__pycache__" ./ unimac1:/Volumes/work/admin/packages/vasputils/
+	rsync -av --delete --exclude=".*" --exclude="__pycache__" ./ unimac1:/Volumes/work/admin/packages/vasputils/
+	rsync -av --delete --exclude=".*" --exclude="__pycache__" ./ kagayaki:~/vasputils/
